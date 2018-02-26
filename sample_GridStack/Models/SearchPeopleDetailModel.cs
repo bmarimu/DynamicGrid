e7578deb7d7636515966f4a9e32207b8981ab11f
Balaji Marimuthu <mbalaji@ceiamerica.com>
2018-02-26 12:22:18 -0500

    Added Search Details

---
+++ sample_GridStack/Models/SearchPeopleDetailModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample_GridStack.Models
+{
+    public class SearchPeopleDetailModel
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string Department { get; set; }
+        public string DepartmentNumber { get; set; }
+        public string WorkLocation { get; set; }
+        public string Group { get; set; }
+
+        public string Email { get; set; }
+        public string OfficePhone { get; set; }
+        public string MailDrop { get; set; }
+        public string AlternateContact { get; set; }
+        public string CellPhone { get; set; }
+        public string FaxNumber { get; set; }
+        public string OrgChartLink { get; set; }
+        public string ProfileSuccessfactorsLink { get; set; }
+    }
+}
